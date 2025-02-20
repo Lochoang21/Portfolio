31f4bdad63fa9e8018839fe7e480e468037c3ae5
LocHoang1 <lochoang2101@gmail.com>
2025-02-20 16:05:58 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "export",
-    images: {
-        unoptimized: true,
-    },
+  output: "export", // Xuất tĩnh để dùng với GitHub Pages
+  basePath: "/Portfolio", // Thay bằng tên repository của bạn
+  assetPrefix: "/Portfolio/", // Đảm bảo CSS và JS được load đúng
+  images: {
+    unoptimized: true, // Bắt buộc nếu đang dùng Next.js Image
+  },
 };
 
 export default nextConfig;
